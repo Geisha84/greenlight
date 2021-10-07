b454a6b3f36c97fe69e966f94277802e100a23ae
Gijs van Bavel <52-gvbadmin@users.noreply.gitlab.cmpayments.local>
2021-10-07 08:34:09 +0200

    Quoted password in input struct

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -11,7 +11,7 @@ import (
 func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Email    string `json:"email"`
-		Password string `json:password`
+		Password string `json:"password"`
 	}
 
 	err := app.readJSON(w, r, &input)
