e3627f4c1e244e0bee27dcc4322a38f7c62406a9
Gijs van Bavel <52-gvbadmin@users.noreply.gitlab.cmpayments.local>
2021-10-18 09:10:39 +0200

    Building Binaries

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -7,8 +7,8 @@ import (
 	"geisha84/greenlight/internal/data"
 	"geisha84/greenlight/internal/validator"
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -58,11 +58,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.RealIP(r)
 
 			mu.Lock()
 
